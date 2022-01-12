fae977cd735f06bf62a846f8bebef031768e724a
BlackWolf96 <oskar.blackwolf@gmail.com>
2022-01-12 02:03:47 +0100

    Update

---
+++ assets/frontend/components/Add.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <div class="form-wrapper">
+          <form>
+              <input type="text" name="autor" />
+              <input type="text" name="tresc" />
+              <input type="data" name="data" />
+              <input type="url" name="link" />
+          </form>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
